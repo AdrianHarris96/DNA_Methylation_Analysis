eb787ea368d8732714d1cac11c10740be9282b88
AdrianHarris96 <97938048+AdrianHarris96@users.noreply.github.com>
2022-06-15 12:25:47 -0400

    Sort Files v1.02
    Removal of sample sheet code and correction to version number

--- sort_array_type.py
+++ sort_array_type.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-#Example input: ./sort_array_type.py -x /Users/adrianharris/Downloads/Kidney\ Tx\ sample\ annotation.xlsx
+#Example input: ./sort_array_type.py -x /Users/adrianharris/Desktop/kidney/kidney\ Tx\ sample\ annotation.xlsx -s 1 -e 5
 
 import pandas as pd
 import argparse as ap
@@ -8,37 +8,27 @@ import os
 import shutil 
 
 parser = ap.ArgumentParser()
-parser = ap.ArgumentParser(description='Calculate effect dose for each UKBB individual using the matrix')
+parser = ap.ArgumentParser(description='Sort raw files into respective folder according to array type')
 parser.add_argument("-x", "--excel", help="excel sample file", required=True)
 parser.add_argument("-w", "--working", help="working directory", default='/Users/adrianharris/Desktop/kidney/')
-parser.add_argument("-o", "--output", help="output csv filename", default='methylation.csv')
+parser.add_argument("-s", "--start", help="starting sheet(#) in excel file", type=int, required=True)
+parser.add_argument("-e", "--end", help="ending sheet(#) in excel file", type=int, required=True)
 args = parser.parse_args()
 
 arrayDict = {} #Dictionary intended for moving files later in script
 
-df = pd.DataFrame(columns=['sample_id', 'sample_well', 'sample_plate', 'sentrix_ID', 'sentrix_position', 'time', 'array_type', 'gender_donor', 'gender_recipient'])
-for x in range(1, 5): #Number of sheets within excel file 
+for x in range(args.start, args.end): #Iterate through important sheets in the excel file 
 	sheet_df =  pd.read_excel(args.excel, sheet_name=x, keep_default_na=True)
 	sheet_df.columns = [col.strip() for col in sheet_df.columns] #Stripping whitespace in column names
-	if x == 4: #Used for the extraction of the sample_id from the excel file 
-		x = 12
-	sample_id = 'SampleID_K' + str(x)
-	for index in sheet_df.index: #iterate through columns
-		row = sheet_df.loc[index, [sample_id, 'Sample_Well', 'Sample_Plate', 'Sentrix_ID', 'Sentrix_Position', 'Time', 'Array_type', 'gender_donor', 'gender_recipient']]
-		row = list(row)
+	for index in sheet_df.index:
 		sentrix = str(sheet_df['Sentrix_ID'][index]) + '_' + str(sheet_df['Sentrix_Position'][index])
 		array = sheet_df['Array_type'][index]
 		if sentrix in arrayDict.keys():
-			pass
-			#print('duplicate')
+			pass #avoid loading duplicates with this line
 		else:
 			arrayDict[sentrix] = array
-		df.loc[len(df) + 1,] = row #adding to df
 
 #print(arrayDict)
-
-df = df[df['time'].notna()] #drop if time is empty
-#print(df)
 
 #Appending to either 450K or EPIC list 
 list450K = []
@@ -54,9 +44,8 @@ for key in arrayDict.keys():
 #print(len(list450K))
 #print(len(listEPIC))
 
-#Creation of new sample sheet (CSV) using output argument
+#Move to working directory and sort files into respective folders 
 os.chdir(args.working)
-df.to_csv(args.output)
 
 #Determine all files
 f = []
