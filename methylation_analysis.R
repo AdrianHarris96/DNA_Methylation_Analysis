e13884086493f5e7d4402f05a321bb6051fdc4bf
AdrianHarris96 <97938048+AdrianHarris96@users.noreply.github.com>
2022-06-22 16:12:54 -0400

    Methylation Analysis v1.06
    Removal of hard-coded paths to increase flexibility of script.

--- methylation_analysis.R
+++ methylation_analysis.R
@@ -1,31 +1,29 @@
 #Script for DNA Methylation Analysis
 rm(list = ls())
+dev.off()
 library(minfi)
-library(shinyMethyl)
-library(MEAL)
 library(rio)
 library(ggplot2)
 library(tidyverse)
 library(RColorBrewer)
-library(lumi)
+
+#Example input: Rscript methylation_analysis.R <pheno_file> <base_dir> <output_dir>
+args=commandArgs(trailingOnly=TRUE)
+pheno_file = args[1]
+base_dir = args[2]
+output_dir = args[3]
+
+if (file.exists(output_dir)) {
+  cat("Directory already exists")
+} else {
+  dir.create(output_dir)
+}
 
 #Importing manually-curated sample sheet
-pheno_df <- import('/Users/adrianharris/Desktop/kidney/kidneyTx_methylation.csv')
+pheno_df <- import(pheno_file)
 
-#Adding new column - sample name
-pheno_df['Basename'] <- 'NA'
-for (row in 1:nrow(pheno_df)) {
-  sample_name <- paste(as.numeric(pheno_df[row, 'sentrix_ID']), pheno_df[row, 'sentrix_position'], sep="_")
-  pheno_df[row, 'Basename'] <- sample_name
-  rm(sample_name)
-}
-rm(row)
-
-#Removal of the V1 column, moving "sample_name" column to front, ordering that column
-pheno_df <- pheno_df[,c(2:(ncol(pheno_df)))]
-pheno_df <- pheno_df[,c(ncol(pheno_df),1:(ncol(pheno_df)-1))]
-#pheno_df <- pheno_df[order(pheno_df$sample_name),]
-#pheno_df <- pheno_df[!duplicated(pheno_df$sample_name),]
+#Must remove outlier sample, 203504430032_R01C01 (and its paired sample 203504430032-R02C01)
+pheno_df <- pheno_df[!(pheno_df$Basename == '203504430032_R01C01' | pheno_df$Basename == '203504430032_R02C01'),]
 
 nrow(subset(pheno_df, array_type == '450K'))
 nrow(subset(pheno_df, array_type == 'EPIC'))
@@ -34,18 +32,21 @@ nrow(subset(pheno_df, array_type == 'EPIC'))
 pheno450k <- pheno_df[!(pheno_df$array_type == 'EPIC'),]
 phenoEPIC <- pheno_df[!(pheno_df$array_type == '450K'),]
 
-#Read data in 
-dir450k <- "/Users/adrianharris/Desktop/kidney/450k_array"
-dirEPIC <- "/Users/adrianharris/Desktop/kidney/EPIC_array"
+#Specify the directories and read in respective IDAT Files 
+dir450k <- paste(base_dir, "450k_array", sep="")
+dirEPIC <- paste(base_dir, "EPIC_array", sep="")
+#Be sure to change target based on comparison 
 rgSet450k <- read.metharray.exp(base=dir450k, target=pheno450k)
 rgSetEPIC <- read.metharray.exp(base=dirEPIC, target=phenoEPIC, force=TRUE)
 rgSet <- combineArrays(rgSet450k, rgSetEPIC)
 #Saving set as an RDS file 
-saveRDS(rgSet450k, file = "/Users/adrianharris/Desktop/kidney/rgSet450k.RDS")
-saveRDS(rgSetEPIC, file = "/Users/adrianharris/Desktop/kidney/rgSetEPIC.RDS")
-saveRDS(rgSet, file = "/Users/adrianharris/Desktop/kidney/rgSet.RDS")
+saveRDS(rgSet450k, file = paste(output_dir, "rgSet450k.RDS", sep=""))
+saveRDS(rgSetEPIC, file = paste(output_dir, "rgSetEPIC.RDS", sep=""))
+saveRDS(rgSet, file = paste(output_dir, "rgSet.RDS", sep=""))
 rm(rgSet450k)
 rm(rgSetEPIC)
+
+q()
 
 #Calculate Detection p-values
 detP <- detectionP(rgSet)
@@ -53,53 +54,54 @@ detP <- data.frame(detP)
 sample_names <- colnames(detP)
 sample_names <- substr(sample_names, 2, nchar(sample_names))
 detPmeans <- colMeans(detP)
-detP_df <- data.frame(matrix(ncol=2, nrow=235))
+detP_df <- data.frame(matrix(ncol=2, nrow=ncol(detP)))
 detP_df['X1'] <- sample_names
 detP_df['X2'] <- detPmeans
-colnames(detP_df) <- c('sample_name', 'p_value_mean')
-#write.csv(detP_df,"/Users/adrianharris/Desktop/kidneyTx_p-values.csv", row.names = FALSE)
+colnames(detP_df) <- c('Basename', 'p_value_mean')
+write.csv(detP_df,"/Users/adrianharris/Desktop/kidneyTx_p-values.csv", row.names = FALSE)
+rm(sample_names, detPmeans)
 
-#Drop the top row before plotting barplot
-detP_df<- detP_df[order(-detP_df$p_value_mean),]
-detP_df<- detP_df[2:nrow(detP_df),]
-detP_df <- merge(detP_df, pheno_df, by = 'sample_name')
+#Merging to pheno dataframe for color coding and plot via barplots 
+detP_df <- merge(detP_df, pheno_df, by = 'Basename')
+
+#Plotting 450K barplot 
 pal <- brewer.pal(4,"Dark2")
 par(mfrow=c(2,1))
-#Plotting 450K barplot 
 barplot((subset(detP_df, array_type == '450K'))$p_value_mean, col=pal[factor(detP_df$time)], names.arg=(subset(detP_df, array_type == '450K'))$sample_name, las=2, cex.names=0.4, cex.axis=0.5, space=0.5, ylab="Mean detection p-values", main='450K Array')
 legend("topleft", legend=levels(factor(detP_df$time)), fill=pal,
        cex=0.27, bty = "n", bg="white")
+
 #Plotting EPIC barplot
 barplot((subset(detP_df, array_type == 'EPIC'))$p_value_mean, col=pal[factor(detP_df$time)], names.arg=(subset(detP_df, array_type == 'EPIC'))$sample_name, las=2, cex.names=0.4, cex.axis=0.5, space=0.5, ylab="Mean detection p-values", main='EPIC Array')
 legend("topleft", legend=levels(factor(detP_df$time)), fill=pal,
        cex=0.27, bty = "n", bg="white")
 
+rm(detP_df)
 par(mfrow=c(1,1))
-#preprocessing QC 
+
+#preprocessing QC and plotting
 mtSet <- preprocessRaw(rgSet)
 qc <- getQC(mtSet)
-plotQC(qc)
-#?plotQC
+#plotQC(qc)
 qc <- data.frame(qc)
-qc['sample_name'] <- row.names(qc)
-qc <- merge(qc, pheno_df, by = 'sample_name')
+qc['Basename'] <- row.names(qc)
+qc <- merge(qc, pheno_df, by = 'Basename')
 plot <- ggplot(data=qc, mapping = aes(x = mMed, y = uMed, color=time)) + geom_point(aes(shape=array_type), alpha=0.5) + xlim(7, 14) + ylim(7, 14) + theme_bw()+ geom_abline(slope=-1, intercept = 21.25 , color="black", linetype="dashed", size=0.5) + scale_color_manual(values=pal)
 print(plot)
 densityPlot(mtSet, sampGroups =qc$array_type)
-rm(detP_df)
-rm(mtSet)
-rm(qc)
+rm(mtSet, qc, plot)
 
-#Normalization
+#Normalization and plotting 
 mtSet <- preprocessNoob(rgSet)
 saveRDS(mtSet, file = "/Users/adrianharris/Desktop/kidney/mtSet.RDS")
 postqc <- getQC(mtSet)
 postqc <- data.frame(postqc)
-postqc['sample_name'] <- row.names(postqc)
-postqc <- merge(postqc, pheno_df, by = 'sample_name')
+postqc['Basename'] <- row.names(postqc)
+postqc <- merge(postqc, pheno_df, by = 'Basename')
 plot2 <- ggplot(data=postqc, mapping = aes(x = mMed, y = uMed, color=time)) + geom_point(aes(shape=array_type), alpha=0.5) + xlim(5, 14) + ylim(5, 14) + theme_bw()+ geom_abline(slope=-1, intercept = 21.25 , color="black", linetype="dashed", size=0.5) + scale_color_manual(values=pal)
 print(plot2)
 densityPlot(mtSet, sampGroups = postqc$array_type)
+rm(postqc, plot2)
 
 # Map to Genome
 rSet <- ratioConvert(mtSet, what = "both", keepCN = TRUE)
@@ -108,19 +110,18 @@ saveRDS(gmtSet, file = "/Users/adrianharris/Desktop/kidney/gmtSet.RDS")
 dim(gmtSet) #Number of probes = 452453
 
 #Predicted Sex 
-predictedSex <- getSex(gmtSet, cutoff = -2)$predictedSex
-
-#Add column to pheno_df
-postqc['predicted_sex'] <- predictedSex
-predictedsex_file <- postqc %>% select(c(sample_id, sample_name, predicted_sex, gender_donor, gender_recipient))
-write.csv(predictedsex_file,"/Users/adrianharris/Desktop/kidneyTx_methylation_predictedSex.csv", row.names = FALSE)
+predictedSex <- getSex(gmtSet, cutoff = -2)
+gmtSet <- addSex(gmtSet, sex = predictedSex)
+plotSex(gmtSet, id = row.names(predictedSex))
+rm(predictedSex)
 
 #Removing probes that include SNPs
 snps <- getSnpInfo(gmtSet)
 gmtSet <- addSnpInfo(gmtSet)
 gr <- granges(gmtSet)
 gmtSet <- dropLociWithSnps(gmtSet, snps=c("SBE","CpG"), maf=0)
-dim(gmtSet) #Number of probes = 436144 
+dim(gmtSet) #Number of probes = 436144
+rm(snps, gr)
 
 # Filter unwanted sites 
 # ensure probes are in the same order in the mSetSq and detP objects
@@ -131,37 +132,39 @@ detP_df <- data.frame(detP)
 keep <- detP < 0.01
 keep.probes <- rownames(detP[rowMeans(keep)>=0.5,]) #probes that failed detection in more than half of the samples
 gmtSet <- gmtSet[keep.probes,] 
-dim(gmtSet) #Number of probes = 436127 
+dim(gmtSet) #Number of probes = 436128
+rm(keep.probes)
 
 #Remove probes that located on the X or Y chromosome
 ann450k <- getAnnotation(IlluminaHumanMethylation450kanno.ilmn12.hg19)
 
-keep <- !(featureNames(gmtSet) %in% ann450k$Name[ann450k$chr %in% 
-                                                   c("chrX","chrY")]) #remove probes that are not of the chrom x or y
+keep <- !(featureNames(gmtSet) %in% ann450k$Name[ann450k$chr %in% c("chrX","chrY")]) #remove probes that are not of the chrom x or y
 table(keep)
 gmtSet <- gmtSet[keep,]
-dim(gmtSet) #Number of probes = 425716
+dim(gmtSet) #Number of probes = 425718
+rm(ann450k, keep)
 
 #Creation of bad probes character and filter gmtSet
-cross.react <- read.csv('/Users/adrianharris/Downloads/illumina450k_filtering/48639-non-specific-probes-Illumina450k.csv', head = T, as.is = T)
+cross.react <- read.csv('/Users/adrianharris/Desktop/kidney/illumina450k_filtering/48639-non-specific-probes-Illumina450k.csv', head = T, as.is = T)
 cross.react.probes <- as.character(cross.react$TargetID)
 #Probes identified with potential hybridization issues
-multi.map <- read.csv('/Users/adrianharris/Downloads/illumina450k_filtering/HumanMethylation450_15017482_v.1.1_hg19_bowtie_multimap.txt', head = F, as.is = T)
+multi.map <- read.csv('/Users/adrianharris/Desktop/kidney/illumina450k_filtering/HumanMethylation450_15017482_v.1.1_hg19_bowtie_multimap.txt', head = F, as.is = T)
 multi.map.probes <- as.character(multi.map$V1)
 # determine unique probes between the cross-reactive and multi-map probes
 bad.probes <- unique(c(cross.react.probes, multi.map.probes))
 keep <- !(featureNames(gmtSet) %in% bad.probes) #Removal of these bad probes
 table(keep)
 gmtSet <- gmtSet[keep,]
-dim(gmtSet) #Number of probes = 392870
+dim(gmtSet) #Number of probes = 392871
+rm(cross.react, multi.map, bad.probes, cross.react.probes, multi.map.probes, keep)
 
-#Grab Betas and m_values
+#Extract betas and m_values
 beta_values <- getBeta(gmtSet)
 m_values <- getM(gmtSet)
 
-#Remove probes Hypomethylated in all samples identified by CpG sites having beta < 0.05 in all samples�0.05 in all samples
+#Remove probes Hypomethylated in all samples identified by CpG sites having beta < 0.05 in all samples
 beta_values_filtered <- as.data.frame(beta_values) 
-dim(filter_all(beta_values_filtered, all_vars(. < 0.05)))
+dim(filter_all(beta_values_filtered, all_vars(. < 0.05))) #Number of hypomethylated
 beta_values_filtered <- filter_all(beta_values_filtered, any_vars(. >= 0.05)) 
 #6327 Hypomethylated
 
@@ -186,26 +189,21 @@ m_values_case = beta2m(beta_values_case)
 
 
 #Transpose resulting PCA plot 
-transposed_beta <- t(beta_values_case)
+transposed_beta <- t(beta_values_filtered) #t(beta_values_case)
 transposed_beta <- data.frame(transposed_beta)
 pca_general <- prcomp(transposed_beta, center=TRUE)
 var_explained <- pca_general$sdev^2/sum(pca_general$sdev^2)
 scores = as.data.frame(pca_general$x)
-scores['sample_name'] <- row.names(scores)
-scores <- merge(scores, pheno_df, by = 'sample_name')
-plot <- ggplot(data=scores, mapping = aes(x = PC1, y = PC2)) +theme_bw() + geom_point()+ labs(x=paste0("PC1: ",round(var_explained[1]*100,1),"%"), y=paste0("PC2: ",round(var_explained[2]*100,1),"%"))
-plot + geom_text(aes(label = sample_name), size=3.5) + xlim(-100, 400)
+scores['Basename'] <- row.names(scores)
+scores <- merge(scores, pheno_df, by = 'Basename')
+pal <- brewer.pal(4,"Dark2")
+plot <- ggplot(data=scores, mapping = aes(x = PC1, y = PC2, color=time)) +theme_bw() + geom_point(aes(shape=eGFR_1month), alpha=0.5)+ labs(x=paste0("PC1: ",round(var_explained[1]*100,1),"%"), y=paste0("PC2: ",round(var_explained[2]*100,1),"%")) + scale_color_manual(values=pal)
+#plot + geom_text(aes(label = sample_name), size=3.5) + xlim(-100, 400)
 print(plot)
 
-#Filter out the outlier sample - 203504430032_R01C01
-filtered_transposed_beta <- transposed_beta[!((row.names(transposed_beta)) %in% '203504430032_R01C01'),]
-pca_general <- prcomp(filtered_transposed_beta, center=TRUE)
-var_explained <- pca_general$sdev^2/sum(pca_general$sdev^2)
-scores = as.data.frame(pca_general$x)
-scores['sample_name'] <- row.names(scores)
-scores <- merge(scores, pheno_df, by = 'sample_name')
-plot <- ggplot(data=scores, mapping = aes(x = PC1, y = PC2, color=time)) +theme_bw() + geom_point(aes(shape=array_type), alpha=0.5)+ labs(x=paste0("PC1: ",round(var_explained[1]*100,1),"%"), y=paste0("PC2: ",round(var_explained[2]*100,1),"%")) + scale_color_manual(values=pal)
-print(plot)
+#Limit S4 object here based on comparisons
+library(MEAL)
+
 
 #Other values you can extract
 M <- getM(gmtSet)
@@ -250,7 +248,7 @@ res <- runPipeline(set = gmtSet, variable_names = "time", analyses = c("DiffMean
 design <- model.matrix(~ gmtSet$time)
 res
 names(res)
-?()
+
 gmtSet <- gmtSet[!(gmtSet$time == "K12" | gmtSet$time == "K3")]
 gmtSet$time
 #Plotting 
