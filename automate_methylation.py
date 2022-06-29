64d837a2f54f6d8b9d904e522bcd0b56ee9fd75f
AdrianHarris96 <97938048+AdrianHarris96@users.noreply.github.com>
2022-06-29 11:28:29 -0400

    Automate Methylation Analysis v1.01
    Addition of output directory flag

--- automate_methylation.py
+++ automate_methylation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-#Example input: ./automate_methylation.py -w /home/amharris/dna_methylation_analysis/
+#Example input: ./automate_methylation.py -w /home/amharris/dna_methylation_analysis/ -o /local/projects-t3/XVMAS_Lab/Projects_2022/XVMAS_P09_methylation/01_analysis/kidneyTx_methylation/kidney_output2/
 
 import subprocess as sp
 import argparse as ap
@@ -8,6 +8,7 @@ import argparse as ap
 parser = ap.ArgumentParser()
 parser = ap.ArgumentParser(description='automate the methylation analysis')
 parser.add_argument("-w", "--working", help="working directory with comparison sheets", required=True)
+parser.add_argument("-o", "--output", help="output directory", required=True)
 args = parser.parse_args()
 
 comp_sheets = ['comp1.csv', 'comp2.csv', 'comp3.csv']
@@ -17,4 +18,4 @@ comp_list = ['K1_Low_K1_High', 'K2_Low_K2_High', 'K1_High_K2_High', 'K1_Low_K2_L
 for sheet in comp_sheets:
 	path = args.working + sheet
 	for comp in comp_list:
-		autorun = sp.call(['Rscript', 'methylation_analysis.R', path, '/local/projects-t3/XVMAS_Lab/Projects_2022/XVMAS_P09_methylation/01_analysis/kidneyTx_methylation/', '/home/amharris/dna_methylation_analysis/', '/local/projects-t3/XVMAS_Lab/Projects_2022/XVMAS_P09_methylation/01_analysis/kidneyTx_methylation/kidney_output/', comp])
+		autorun = sp.call(['Rscript', 'methylation_analysis.R', path, '/local/projects-t3/XVMAS_Lab/Projects_2022/XVMAS_P09_methylation/01_analysis/kidneyTx_methylation/', '/home/amharris/dna_methylation_analysis/', args.output, comp])
