aa1074eea6db6dcfd76c8c513a11ea4fae03698d
AdrianHarris96 <97938048+AdrianHarris96@users.noreply.github.com>
2022-07-09 19:35:49 -0400

    Sort Liver samples for analysis v1.0
    Shell script to sort the liver samples prior to analysis

---
+++ sort_liver_files.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+#Example input: ./sort_liver_samples.sh -w <base_dir> -o <out_dir>
+
+function HELP {
+	echo "The Sort Liver Samples shell script requires flags, -w and -o for the path to the base directory of files, and the desired output directory, respectively."
+	exit 2
+}
+
+while getopts "b:o:v" option; do 
+	case $option in
+		b) base_dir=$OPTARG;;
+		o) out_dir=$OPTARG;;
+		v)set -x;;
+		\?) HELP;;
+	esac
+done
+
+let start_time="$(date +%s)"
+echo "$base_dir is the base directory"
+echo "$out_dir is the output directory"
+
+#Append files from file directory to file list
+file_list=()
+for FILE in $base_dir/*;
+do 
+	extension="${FILE##*/}"
+	echo "$extension"
+	#if [[ $extension == *"CGT"* ]]; then
+		#file_list+="$FILE/$extension.fa "
+	#fi
+done
+
+#Move files from deep into base directory to the output directory
