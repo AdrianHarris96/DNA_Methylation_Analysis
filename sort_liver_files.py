5ad7c35be7c0dd4715e641143433b7f789e894e2
AdrianHarris96 <97938048+AdrianHarris96@users.noreply.github.com>
2022-07-10 20:05:47 -0400

    Sort liver files (Python) v1.0
    Sort liver files using python

---
+++ sort_liver_files.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python3
+
+#Example input: ./sort_liver_files.py -b <base_dir> -o <out_dir>
+
+import pandas as pd
+import argparse as ap
+import os
+import shutil 
+
+parser = ap.ArgumentParser()
+parser = ap.ArgumentParser(description='Sort raw liver files')
+parser.add_argument("-b", "--base_dir", help="base directory", required=True)
+parser.add_argument("-o", "--out_dir", help="output directory", required=True)
+args = parser.parse_args()
+
+#Move to working directory and sort files into respective folders 
+os.chdir(args.base_dir)
+
+#Append to dir_list
+dir_list = []
+for directory in os.listdir(args.base_dir):
+    dir_list.append(directory)
+
+#Iterate through directories
+file_list = []
+for direct in dir_list:
+	for data in os.listdir(direct):
+		if ".idat" in data:
+			file_list.append(data)
+
+print(dir_list)
+print('\n')
+print(file_list)
+
+#Moving to corresponding directory based on array_type
+#for item in list450K:
+	#for file in f:
+		#if item in file:
+			#src = args.working + file
+			#dst = args.directory + '450k_array/' + file
+			#shutil.copyfile(src, dst)
+
+
+
