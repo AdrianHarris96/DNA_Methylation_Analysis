781ddf310358f4d1e9d5651d0ad92c8f66fadd7f
AdrianHarris96 <97938048+AdrianHarris96@users.noreply.github.com>
2022-07-16 21:53:58 -0400

    Liver methylation analysis v2.04
    Use betas with dmpFinder instead

--- liver_methylation_analysis.R
+++ liver_methylation_analysis.R
@@ -488,8 +488,8 @@ for (comp in comparisons) {
   pheno <- rbind(pheno1, pheno2)
   print(dim(pheno))
   
-  m_values_condition <- m_values[,(colnames(m_values) %in% pheno$Basename)]
-  m_values_condition <- as.matrix(m_values_condition)
+  betas_condition <- beta_values_filtered[,(colnames(beta_values_filtered) %in% pheno$Basename)]
+  betas_condition <- as.matrix(betas_condition)
   
   if (comp == 'DD_HI_L1-DD_HI_L2') {
     condition <- pheno$collection
@@ -511,11 +511,11 @@ for (comp in comparisons) {
     condition <- pheno$collection
   }
   
-  DMPs <- dmpFinder(m_values_condition, pheno=condition, type = "categorical")
+  DMPs <- dmpFinder(betas_condition, pheno=condition, type = "categorical")
   DMPs$adj_p <- p.adjust(DMPs$pval, method="BH")
   print(head(DMPs, n = 10L))
 
-  ann450kSub <- ann450k[match(rownames(m_values_condition),ann450k$Name), c(1:4,12:19,24:ncol(ann450k))]
+  ann450kSub <- ann450k[match(rownames(betas_condition),ann450k$Name), c(1:4,12:19,24:ncol(ann450k))]
   ann450kSub <- data.frame(ann450kSub)
   
   DMPs <- merge(DMPs, ann450kSub, by = "row.names")
