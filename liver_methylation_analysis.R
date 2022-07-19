127786667052711cc0d5507566e278725b306042
AdrianHarris96 <97938048+AdrianHarris96@users.noreply.github.com>
2022-07-19 16:26:44 -0400

    Liver methylation analysis - combined arrays v3.05
    Comment out reference to local machine

--- liver_methylation_analysis.R
+++ liver_methylation_analysis.R
@@ -15,7 +15,7 @@ git_dir = args[3]
 output_dir = args[4]
 
 #Local Machine
-pheno_file = '/Users/adrianharris/Documents/dna_methylation_analysis/liver_sample_sheet.csv'
+# pheno_file = '/Users/adrianharris/Documents/dna_methylation_analysis/liver_sample_sheet.csv'
 # base_dir = '/Users/adrianharris/Desktop/liver/'
 # git_dir = '/Users/adrianharris/Documents/dna_methylation_analysis/'
 # output_dir = '/Users/adrianharris/Desktop/liver/'
