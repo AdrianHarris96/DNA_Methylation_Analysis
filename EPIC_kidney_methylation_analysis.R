27d40258736c905ac1bc0546cdfbc84e1e60ae56
AdrianHarris96 <97938048+AdrianHarris96@users.noreply.github.com>
2022-07-25 10:14:37 -0400

    EPIC-ONLY methylation analysis for kidney v2.18
    Checking order

--- EPIC_kidney_methylation_analysis.R
+++ EPIC_kidney_methylation_analysis.R
@@ -235,11 +235,10 @@ paired_pheno <- paired_pheno[!(paired_pheno$sample_id == 'KUT4_K2' | paired_phen
 
 #Making the phenotype dataframe the paired dataframe 
 pheno_df <- pheno_df %>% select(c(Basename))
-pheno_df <- merge(pheno_df, paired_pheno, by = 'Basename')
+pheno_df <- merge(paired_pheno, pheno_df, by = 'Basename')
 beta_values_filtered <- beta_values_filtered[,(colnames(beta_values_filtered) %in% pheno_df$Basename)]
 print(pheno_df$Basename)
 print(colnames(beta_values_filtered))
-q()
 
 #Exclude DCD samples - Kidney data
 beta_values_filtered <- beta_values_filtered[,!(colnames(beta_values_filtered) %in% c("201465900002_R04C01", "202259350016_R08C01", "202259340119_R05C01", "203504430032_R05C01", "203496240002_R03C01", "202259340119_R06C01", "203496240002_R04C01", "203504430032_R06C01"))]
