a7ed1355139a4a171c25a165f95c22bb1c34ded9
AdrianHarris96 <97938048+AdrianHarris96@users.noreply.github.com>
2022-08-02 16:52:32 -0400

    Calculate biological age using DNAm v1.01
    Addition of code to evaluate paired liver samples

--- calc_bio_age.R
+++ calc_bio_age.R
@@ -18,7 +18,7 @@ pheno_df <- import('/Users/adrianharris/Documents/dna_methylation_analysis/paire
 age <- age %>% rename(Basename = id)
 
 pheno_df <- merge(pheno_df, age, by = 'Basename')
-pheno_df <- subset(pheno_df, time == 'K1')
+# pheno_df <- subset(pheno_df, time == 'K1')
 
 x1 <- pheno_df$donor_age
 y1 <- pheno_df$Horvath
@@ -27,6 +27,69 @@ plot <- ggplot(data=pheno_df, mapping = aes(x = x1, y = y1, color=eGFR_1month)) 
 plot <- ggplot(data=pheno_df, mapping = aes(x = x1, y = y1, color=eGFR_1month)) + theme_bw() + geom_text(aes(label = sample_id), size = 2.5) + labs(x='chronological age', y='biological age (DNAm)') + xlim(20, 80) + ylim(20, 80) + ggtitle('kidney - biological v. chronological age')
 plot + geom_abline(slope = 1, intercept=0)
 print(plot)
+
+#Consistency with sample_name
+for (row in 1:nrow(pheno_df)){
+  sample <- pheno_df[row, 'sample_id']
+  sample <- strsplit(sample, "L")
+  sample <- unlist(sample)[1]
+  sample <- str_trim(sample, side="both")
+  collection <- pheno_df[row, 'collection']
+  new_name <- paste(sample, collection, sep = " ")
+  pheno_df[row, 'sample_name'] <- new_name
+}
+
+#SPlitting the pheno_df
+pheno_df1 <- pheno_df[(pheno_df$collection == 'L1'),]
+pheno_df2 <- pheno_df[(pheno_df$collection == 'L2'),]
+
+pheno_df1 <- pheno_df1[!(pheno_df1$sample_name == 'V037 L1'),]
+
+#Preparing for the merge by creating pair column
+for (row in 1:nrow(pheno_df1)) {
+  common <- pheno_df1[row, 'sample_name']
+  common <- substr(common,1,nchar(common)-1)
+  pheno_df1[row, 'pair'] <- common
+}
+
+for (row in 1:nrow(pheno_df2)) {
+  common <- pheno_df2[row, 'sample_name']
+  common <- substr(common,1,nchar(common)-1)
+  pheno_df2[row, 'pair'] <- common
+}
+
+#Merging on pair
+final_df <- merge(pheno_df1, pheno_df2, by = 'pair')
+
+final_df$diff <- 'na'
+for (row in 1:nrow(final_df)) {
+  diff <- final_df[row, 'Horvath.y'] - final_df[row, 'Horvath.x']
+  final_df[row, 'diff'] <- diff
+}
+
+L1 <- final_df$Horvath.x
+L2 <- final_df$Horvath.y
+group <- final_df$sample_group.x
+d <- data.frame(L1 = L1, L2 = L2, group = group)
+
+for (row in 1:nrow(d)){
+  if (d[row, 'group'] == "High Injury") {
+    d[row, 'group'] <- "red"
+  } else {
+    d[row, 'group'] <- "blue"
+  }
+}
+
+x <- factor(d$group)
+y <- append(x, x)
+library(ggpubr)
+ggpaired(d, cond1 = "L1", cond2 = "L2", width = 0.1, point.size = 1.0, line.size = 0.3, line.color = y, ggtheme=theme_bw(), title="Changes in biological age (DNAm) - Injury Status") + labs(x='collection', y='biological age (DNAm)', color="Legend")
+line.color = "black"
+
+#Rename columns and write to csv
+d <- d %>% rename('L1 Biological Age' = 'L1')
+d <- d %>% rename('L2 Biological Age' = 'L2')
+write.csv(d, file = '/Users/adrianharris/Desktop/liver_bio_age_changes.csv', row.names = FALSE)
 
 #Liver 
 betas <- import('/Users/adrianharris/Desktop/large_files_liver_combined/beta_values.csv')
@@ -40,7 +103,8 @@ age <- age %>% rename(Basename = id)
 pheno_df <- merge(pheno_df, age, by = 'Basename')
 pheno_df <- subset(pheno_df, sample_group != 'control')
 pheno_df <- subset(pheno_df, sample_group != 'Control')
-pheno_df <- subset(pheno_df, collection == 'L1')
+
+# pheno_df <- subset(pheno_df, collection == 'L1')
 
 x1 <- pheno_df$donor_age
 y1 <- pheno_df$Horvath
@@ -49,3 +113,70 @@ plot <- ggplot(data=pheno_df, mapping = aes(x = x1, y = y1, color=sample_group))
 plot <- ggplot(data=pheno_df, mapping = aes(x = x1, y = y1, color=sample_group)) + theme_bw() + geom_text(aes(label = sample_name), size = 2.5) + labs(x='chronological age', y='biological age (DNAm)') + xlim(20, 120) + ylim(20, 120) + ggtitle('liver - biological v. chronological age')
 plot + geom_abline(slope = 1, intercept=0)
 print(plot)
+
+
+#Pairs - liver 
+#Consistency with sample_name
+for (row in 1:nrow(pheno_df)){
+  sample <- pheno_df[row, 'sample_name']
+  sample <- strsplit(sample, "L")
+  sample <- unlist(sample)[1]
+  sample <- str_trim(sample, side="both")
+  collection <- pheno_df[row, 'collection']
+  new_name <- paste(sample, collection, sep = " ")
+  pheno_df[row, 'sample_name'] <- new_name
+}
+
+#SPlitting the pheno_df
+pheno_df1 <- pheno_df[(pheno_df$collection == 'L1'),]
+pheno_df2 <- pheno_df[(pheno_df$collection == 'L2'),]
+
+pheno_df1 <- pheno_df1[!(pheno_df1$sample_name == 'V037 L1'),]
+
+#Preparing for the merge by creating pair column
+for (row in 1:nrow(pheno_df1)) {
+  common <- pheno_df1[row, 'sample_name']
+  common <- substr(common,1,nchar(common)-1)
+  pheno_df1[row, 'pair'] <- common
+}
+
+for (row in 1:nrow(pheno_df2)) {
+  common <- pheno_df2[row, 'sample_name']
+  common <- substr(common,1,nchar(common)-1)
+  pheno_df2[row, 'pair'] <- common
+}
+
+#Merging on pair
+final_df <- merge(pheno_df1, pheno_df2, by = 'pair')
+
+final_df$diff <- 'na'
+for (row in 1:nrow(final_df)) {
+  diff <- final_df[row, 'Horvath.y'] - final_df[row, 'Horvath.x']
+  final_df[row, 'diff'] <- diff
+}
+
+L1 <- final_df$Horvath.x
+L2 <- final_df$Horvath.y
+group <- final_df$sample_group.x
+d <- data.frame(L1 = L1, L2 = L2, group = group)
+
+for (row in 1:nrow(d)){
+  if (d[row, 'group'] == "High Injury") {
+    d[row, 'group'] <- "red"
+  } else {
+    d[row, 'group'] <- "blue"
+  }
+}
+
+x <- factor(d$group)
+y <- append(x, x)
+library(ggpubr)
+ggpaired(d, cond1 = "L1", cond2 = "L2", width = 0.1, point.size = 1.0, line.size = 0.3, line.color = y, ggtheme=theme_bw(), title="Changes in biological age (DNAm) - Injury Status") + labs(x='collection', y='biological age (DNAm)', color="Legend")
+line.color = "black"
+
+#Rename columns and write to csv
+d <- d %>% rename('L1 Biological Age' = 'L1')
+d <- d %>% rename('L2 Biological Age' = 'L2')
+write.csv(d, file = '/Users/adrianharris/Desktop/liver_bio_age_changes.csv', row.names = FALSE)
+
+
