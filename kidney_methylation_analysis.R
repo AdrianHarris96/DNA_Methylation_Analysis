8a90c94f8fb42bf112308ef5557b058aeab378d4
AdrianHarris96 <97938048+AdrianHarris96@users.noreply.github.com>
2022-08-03 10:49:45 -0400

    Kidney methylation analysis - combined v1.12
    Changes to the subset step for CIT

--- kidney_methylation_analysis.R
+++ kidney_methylation_analysis.R
@@ -284,7 +284,7 @@ m_values <- ComBat(dat=m_values, batch=batch, mod=modCombat)
 beta_values_filtered <- data.frame(m2beta(m_values))
 
 #Drop samples lacking CIT information and correct with Combat
-pheno_df <- subset(pheno_df, CIT != '??')
+pheno_df <- subset(pheno_df, CIT != '')
 print(pheno_df)
 q()
 
