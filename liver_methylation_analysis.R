cb943fcab34431c1301d2bb9456bf24cd0dee8b1
AdrianHarris96 <97938048+AdrianHarris96@users.noreply.github.com>
2022-08-11 12:26:05 -0400

    Liver methylation analysis - combined arrays v3.55
    Change age to Horvath in model.matrix - incongruency between what variables to use and the columns in pheno_df was likely driving the error

--- liver_methylation_analysis.R
+++ liver_methylation_analysis.R
@@ -510,11 +510,11 @@ for (row in 1:nrow(pheno_df)) {
 
 #Preparation for model matrix (multiple regression)
 condition <- factor(pheno_df$condition)
-age <- as.numeric(pheno_df$Horvath)
+Horvath <- as.numeric(pheno_df$Horvath)
 
 print('Begin regression model')
 # create design matrix
-design <- model.matrix(~condition+age, data=pheno_df)
+design <- model.matrix(~condition+Horvath, data=pheno_df)
 colnames(design) <- c("DD_HI_L1","DD_HI_L2","DD_LI_L1","DD_LI_L2","LD_LI_L1","LD_LI_L2")
 
 # fit the linear model 
