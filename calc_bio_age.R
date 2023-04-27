e84753028149a8230b4adcc5533234cd9b573585
AdrianHarris96 <97938048+AdrianHarris96@users.noreply.github.com>
2023-04-26 22:49:37 -0400

    Edits to liver section - calculate bio age v1.4.0
    Minor fixes to the liver section, mimicking the workflow of the kidney section

--- calc_bio_age.R
+++ calc_bio_age.R
@@ -81,14 +81,20 @@ ggpaired(d, cond1 = "K1", cond2 = "K2", width = 0.1, point.size = 1.0, line.size
 line.color = "black"
 
 #Liver section for calculating biological age 
-betas <- import('/Users/adrianharris/Desktop/large_files_liver_combined/beta_values.csv')
+rm(list = ls())
+
+#Re-load betas and metadata
+betas_file <- '/Users/adrianharris/Desktop/Mas_lab/week_08262022/large_files_liver_combined/beta_values.csv'
+metadata_file <- '/Users/adrianharris/Documents/GitHub/DNA_Methylation_Analysis/liver_sample_sheet.csv'
+betas <- import(betas_file)
+
 betas <- betas %>% rename(ProbeID = V1)
-age <- DNAmAge(betas)
+age <- DNAmAge(betas) #calculate biological age
 
-pheno_df <- import('/Users/adrianharris/Documents/dna_methylation_analysis/liver_sample_sheet.csv')
+pheno_df <- import(metadata_file)
 
+#Formatting and merging with metadata
 age <- age %>% rename(Basename = id)
-
 pheno_df <- merge(pheno_df, age, by = 'Basename')
 pheno_df <- subset(pheno_df, sample_group != 'control')
 pheno_df <- subset(pheno_df, sample_group != 'Control')
@@ -96,12 +102,15 @@ pheno_df <- subset(pheno_df, sample_group != 'Control')
 # pheno_df <- subset(pheno_df, collection == 'L1')
 
 x1 <- pheno_df$donor_age
-y1 <- pheno_df$Horvath
+y1 <- pheno_df$Horvath.y
 
-plot <- ggplot(data=pheno_df, mapping = aes(x = x1, y = y1, color=sample_group)) + theme_bw() + geom_point(alpha=0.9) + labs(x='chronological age', y='biological age (DNAm)') + xlim(20, 120) + ylim(20, 120) + ggtitle('liver - biological v. chronological age')
-plot <- ggplot(data=pheno_df, mapping = aes(x = x1, y = y1, color=sample_group)) + theme_bw() + geom_text(aes(label = sample_name), size = 2.5) + labs(x='chronological age', y='biological age (DNAm)') + xlim(20, 120) + ylim(20, 120) + ggtitle('liver - biological v. chronological age')
+#Plotting via points 
+plot <- ggplot(data=pheno_df, mapping = aes(x = x1, y = y1, color=sample_group)) + theme_bw() + geom_point(alpha=0.9) + labs(x='chronological age', y='biological age (DNAm)') + xlim(20, 80) + ylim(20, 80) + ggtitle('liver - biological v. chronological age')
 plot + geom_abline(slope = 1, intercept=0)
-print(plot)
+
+#Plotting with labels 
+plot1 <- ggplot(data=pheno_df, mapping = aes(x = x1, y = y1, color=sample_group)) + theme_bw() + geom_text(aes(label = sample_name), size = 2.5) + labs(x='chronological age', y='biological age (DNAm)') + xlim(20, 80) + ylim(20, 80) + ggtitle('liver - biological v. chronological age')
+plot1 + geom_abline(slope = 1, intercept=0)
 
 
 #Pairs - liver 
