e67cbd54b99544e7743ce7f354a11c5af88a8845
AdrianHarris96 <97938048+AdrianHarris96@users.noreply.github.com>
2023-04-26 23:02:36 -0400

    Add conditional check in liver section - calculate bio age v1.5.0
    Check if the liver metadata has a column for the biological age already. If so, drop the column and re-calculate.

--- calc_bio_age.R
+++ calc_bio_age.R
@@ -93,6 +93,12 @@ age <- DNAmAge(betas) #calculate biological age
 
 pheno_df <- import(metadata_file)
 
+#Drop Horvath age if it exists in the df
+if ("Horvath" %in% colnames(pheno_df)) {
+  # if it exists, drop it
+  pheno_df <- subset(pheno_df, select = -Horvath)
+}
+
 #Formatting and merging with metadata
 age <- age %>% rename(Basename = id)
 pheno_df <- merge(pheno_df, age, by = 'Basename')
@@ -102,7 +108,7 @@ pheno_df <- subset(pheno_df, sample_group != 'Control')
 # pheno_df <- subset(pheno_df, collection == 'L1')
 
 x1 <- pheno_df$donor_age
-y1 <- pheno_df$Horvath.y
+y1 <- pheno_df$Horvath
 
 #Plotting via points 
 plot <- ggplot(data=pheno_df, mapping = aes(x = x1, y = y1, color=sample_group)) + theme_bw() + geom_point(alpha=0.9) + labs(x='chronological age', y='biological age (DNAm)') + xlim(20, 80) + ylim(20, 80) + ggtitle('liver - biological v. chronological age')
@@ -125,13 +131,14 @@ for (row in 1:nrow(pheno_df)){
   pheno_df[row, 'sample_name'] <- new_name
 }
 
-#SPlitting the pheno_df
+#Splitting the pheno_df
 pheno_df1 <- pheno_df[(pheno_df$collection == 'L1'),]
 pheno_df2 <- pheno_df[(pheno_df$collection == 'L2'),]
 
+#Dropping the single, unpaired sample
 pheno_df1 <- pheno_df1[!(pheno_df1$sample_name == 'V037 L1'),]
 
-#Preparing for the merge by creating pair column
+#Formatting prior to the merge by creating pair column
 for (row in 1:nrow(pheno_df1)) {
   key <- pheno_df1[row, 'sample_name']
   key <- substr(key,1,nchar(key)-1)
@@ -147,17 +154,20 @@ for (row in 1:nrow(pheno_df2)) {
 #Merging on pair
 final_df <- merge(pheno_df1, pheno_df2, by = 'pair')
 
+#Calculating difference across timepoints
 final_df$diff <- 'na'
 for (row in 1:nrow(final_df)) {
   diff <- final_df[row, 'Horvath.y'] - final_df[row, 'Horvath.x']
   final_df[row, 'diff'] <- diff
 }
 
+#Extracting and loading ages into separate df
 L1 <- final_df$Horvath.x
 L2 <- final_df$Horvath.y
 group <- final_df$sample_group.x
 d <- data.frame(L1 = L1, L2 = L2, group = group)
 
+#Mapping colors to the injury status
 for (row in 1:nrow(d)){
   if (d[row, 'group'] == "High Injury") {
     d[row, 'group'] <- "red"
@@ -166,15 +176,10 @@ for (row in 1:nrow(d)){
   }
 }
 
+#Plotting via ggpaired
 x <- factor(d$group)
 y <- append(x, x)
 library(ggpubr)
 ggpaired(d, cond1 = "L1", cond2 = "L2", width = 0.1, point.size = 1.0, line.size = 0.3, line.color = y, ggtheme=theme_bw(), title="Changes in biological age (DNAm) - Injury Status") + labs(x='collection', y='biological age (DNAm)', color="Legend")
 line.color = "black"
-
-#Rename columns and write to csv
-d <- d %>% rename('L1 Biological Age' = 'L1')
-d <- d %>% rename('L2 Biological Age' = 'L2')
-write.csv(d, file = '/Users/adrianharris/Desktop/liver_bio_age_changes.csv', row.names = FALSE)
-
 
